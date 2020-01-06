3c028e3f631958202bc4fb5706da27e88f58c859
ronald <nhonoskywalker@yahoo.com>
2020-01-06 14:18:24 +0800

    routing and importing bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
