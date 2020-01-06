e76064f7ec45e9f5f8d681fcb849c869fe037ff3
ronald <nhonoskywalker@yahoo.com>
2020-01-06 18:48:49 +0800

    quiclink strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -16,9 +18,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
-    preloadingStrategy: PreloadAllModules
-  })],
+  imports: [
+    CommonModule,
+    QuicklinkModule,
+    RouterModule.forRoot(routes, {
+    preloadingStrategy: QuicklinkStrategy
+  })
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
