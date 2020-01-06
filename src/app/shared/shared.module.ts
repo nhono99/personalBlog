c5d4789a9cebd638d24eaa65c4a01b1228b1fc34
ronald <nhonoskywalker@yahoo.com>
2020-01-06 13:21:00 +0800

    initial setup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
